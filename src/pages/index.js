58d32a1eaf6361289fd6dd88ac0804eac0340100
Matias Sauan <matias@psiware.com.ar>
2023-02-16 22:42:25 -0300

    imgs

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,12 @@ import {
 import Image from "next/image";
 import deved from "../../public/dev-ed-wave.png";
 import design from "../../public/design.png";
-/* import code from "../../public/code.png";
-import consulting from "../../public/consulting.png"; */
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import web3 from "../../public/web3.png";
+import web4 from "../../public/web4.png";
+import web5 from "../../public/web5.png";
+import web6 from "../../public/web6.png";
 
 
 export default function Home() {
@@ -135,21 +139,54 @@ export default function Home() {
               old.
             </p>
           </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 relative">
-              <Image className="rounded-lg object-cover fill" width={'100%'} height={'100%'}/*  layout={"fill"} *//>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap relative">
+          <div className="basis-1/3 flex-1 ">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web1}
+              />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image/>
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web2}
+              />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image/>
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web3}
+              />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image/>
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web4}
+              />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image/>
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web5}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web6}
+              />
             </div>
           </div>
         </section>
